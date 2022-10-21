eb7763c7e7a0a1df44eff5d20910c5f821fcd9ad
Norberto Rubulotta <nrubulotta24@gmail.com>
2022-10-21 18:15:58 +0200

    Correccion  desafio 1

--- clase-1/index.js
+++ clase-1/index.js
@@ -1,82 +1,59 @@
 
-const container = [];
-
-class Contenedor {
-
-  constructor(id, title, price, thumbnail) {
-    this.id = id;
-    this.title = title;
-    this.price = price;
-    this.thumbnail = thumbnail;
-  };
+class Container {
+  #products
+  constructor() {
+    this.#products = []
+  }
 
   save(product) {
-    return container.push(product);
+    this.#products.push(product)
   }
 
   getById(id) {
-    const result = container.find(container => container.id == id);
+    const result = this.#products.find(products => products.id === id);
     if (result) {
       return result
     }
     return null
   }
 
+  getAll() {
+    return this.#products
+  }
 
   deleteById(id) {
-  /*   const index = container.indexOf(id);
-
-    console.log(index); */
-
-    container.splice(id-1, 1);
+    const index = this.#products.indexOf(this.#products.find(products => products.id === id));
+    this.#products.splice(index, 1);
   }
-
-  getAll() {
-    return container;
-  }
-
 
   deleteAll() {
-    container.splice(0, container.length);
+    this.#products = []
   }
+}
+
+function updateProducts() {
+  
+  const allProducts = new Container;
+
+  allProducts.save({ id: 1, title: "Ron", price: 18, thumbnail: "www.ron.com" });
+  allProducts.save({ id: 2, title: "Vodka", price: 15, thumbnail: "www.vodka.com" });
+  allProducts.save({ id: 3, title: "Gin", price: 13, thumbnail: "www.gin.com" });
+  allProducts.save({ id: 4, title: "Fernet", price: 21, thumbnail: "www.Fernet.com" });
+
+  let showAll = allProducts.getAll();
+  console.log(showAll);
+
+  const getProduct = allProducts.getById(4);
+  console.log(getProduct);
+
+  allProducts.deleteById(4);
+  showAll = allProducts.getAll();
+  console.log(showAll);
+
+  allProducts.deleteAll();
+  showAll = allProducts.getAll();
+  console.log(showAll);
 
 }
 
-
-let product1 = new Contenedor(1, 'Ron Bacardi', 18, 'IMGron');
-product1.save(product1);
-
-product1 = new Contenedor(2, 'Campari', 22, 'IMGcampari');
-product1.save(product1);
-
-product1 = new Contenedor(3, 'Fernet Branca', 24, 'IMGfernet');
-product1.save(product1);
-
-product1 = new Contenedor(4, 'Aperol', 20, 'IMGaperol');
-product1.save(product1);
-
-product1 = new Contenedor(5, 'Gin', 22, 'IMGgin');
-product1.save(product1);
-
-console.log("Se muestran todos los productos guardados");
-console.log(container);
-
-
-let findProduct = product1.getById(4);
-console.log("Se muestra el producto con el ID seleccionado");
-console.log(findProduct);
-
-
-const allItems = product1.getAll();
-console.log("Se traen todos los productos guardados");
-console.log(allItems);
-
-
-product1.deleteById(1);
-console.log("Se borra el elemento con el ID elegido");
-console.log(container);
-
-product1.deleteAll();
-console.log("Se borran todos los elementos");
-console.log(container);
-
+updateProducts();
