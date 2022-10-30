76e206e8f361004da29a2cf13e491c3d63136864
Norberto Rubulotta <nrubulotta24@gmail.com>
2022-10-30 19:18:45 +0100

    Desafío 3

--- clase-3/server.js
+++ clase-3/server.js
@@ -11,7 +11,7 @@ function procesa(request, response) {
     response.end('Hello there!!☺')
 } */
 
-const http = require('http');
+/* const http = require('http');
 
 const server = http.createServer((peticion, respuesta) => {
     respuesta.end('Hello World!!')
@@ -19,5 +19,32 @@ const server = http.createServer((peticion, respuesta) => {
 
 const connectedServer = server.listen(8080, () => {
     console.log(`Server HTTP listening port number ${connectedServer.address().port} `);
+}) */
+
+const express = require('express');
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('<h1>Servidor desafío 3<h1/>')
 })
+
+app.get('/products', (req, res) => {
+    res.send(`${JSON.stringify(products)}`)
+})
+
+app.get('/randomProduct', (req, res) => {
+    res.send(`${JSON.stringify(products[randomProduct])}`)
+})
+
+function runServer() {
+
+    const server = app.listen(8080, () => {
+        console.log(`Server running on PORT ${server.address().port}`)
+    })
+    server.on("error", error => console.log(`Error en servidor ${error}`))
+}
+
+runServer();
 
